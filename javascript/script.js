d183ef4e59f4553b9cb5ccbd6f554b92fb3f6653
gabykarsten <karstengaby@gmail.com>
2024-02-23 15:10:59 -0800

    added javascript for the options available

--- javascript/script.js
+++ javascript/script.js
@@ -1,26 +1,50 @@
 function updateTime() {
   //los angeles
+
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
-    "h:mm:ss"
-  )} <small>${losAngelesTime.format("A")}</small>`;
-
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
   //Jakarta
-  let jakartaElement = document.querySelector("#jakarta");
-  let jakartaDateElement = jakartaElement.querySelector(".date");
-  let jakartaTimeElement = jakartaElement.querySelector(".time");
-  let jakartaTime = moment().tz("Asia/Jakarta");
 
-  jakartaDateElement.innerHTML = jakartaTime.format("MMMM Do YYYY");
-  jakartaTimeElement.innerHTML = `${jakartaTime.format(
-    "h:mm:ss"
-  )} <small>${jakartaTime.format("A")}</small>`;
+  let jakartaElement = document.querySelector("#jakarta");
+  if (jakartaElement) {
+    let jakartaDateElement = jakartaElement.querySelector(".date");
+    let jakartaTimeElement = jakartaElement.querySelector(".time");
+    let jakartaTime = moment().tz("Asia/Jakarta");
+
+    jakartaDateElement.innerHTML = jakartaTime.format("MMMM Do YYYY");
+    jakartaTimeElement.innerHTML = `${jakartaTime.format(
+      "h:mm:ss"
+    )} <small>${jakartaTime.format("A")}</small>`;
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+                <div>
+                    <h2>${cityName}</h2>
+                    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+                </div>
+                <div class="time">${cityTime.format(
+                  "h:mm:ss"
+                )}<small>${cityTime.format("A")}</small></div>
+            </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
